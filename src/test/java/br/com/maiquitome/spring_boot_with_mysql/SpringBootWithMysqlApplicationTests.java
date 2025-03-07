71d84cd7d691e26fbf12a54c2c5a0573f7908f6e
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-03-07 07:37:07 -0300

    chore: :tada: add initial structure

---
+++ src/test/java/br/com/maiquitome/spring_boot_with_mysql/SpringBootWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maiquitome.spring_boot_with_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
