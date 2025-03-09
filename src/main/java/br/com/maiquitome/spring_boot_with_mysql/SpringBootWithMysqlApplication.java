5948a91dfc525a3988503a302b0d11f23c49b319
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-03-08 21:34:19 -0300

    feat: :sparkles: add `Pessoa` Entity

--- src/main/java/br/com/maiquitome/spring_boot_with_mysql/SpringBootWithMysqlApplication.java
+++ src/main/java/br/com/maiquitome/spring_boot_with_mysql/SpringBootWithMysqlApplication.java
@@ -2,8 +2,10 @@ package br.com.maiquitome.spring_boot_with_mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.maiquitome.spring_boot_with_mysql")
+@EntityScan(basePackages = "br.com.maiquitome.spring_boot_with_mysql.model")
 public class SpringBootWithMysqlApplication {
 
 	public static void main(String[] args) {
